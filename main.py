3562416c6da97a173f682e82baadbae2c0717198
Javier Cañadillas <javiercm@google.com>
2020-09-30 11:00:15 +0200

    Traducido al castellano

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 def hello_world():
     name = os.environ.get('NAME', 'World')
     user_agent = request.headers.get('User-Agent')
-    return '<h1>Hello {}!</h1><p>Brougth to you from your browser {}</p>'.format(name, user_agent)
+    return '<h1>Hola {}!</h1><p>Brougth to you from your browser {}</p>'.format(name, user_agent)
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return '<h1>Hola, {}!</h1>'.format(name)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
