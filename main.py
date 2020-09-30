3a9e38f3b9f7603667432f2fb73f81aedb33e0e3
Javier Cañadillas <javiercm@google.com>
2020-09-30 15:25:38 +0200

    Added templates

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
+import os, socket, platform
 
-from flask import Flask, request
+from flask import Flask, request, make_response, render_template
 
 
 app = Flask(__name__)
@@ -8,12 +8,25 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     name = os.environ.get('NAME', 'World')
+    hostname = socket.gethostname()
+    host_info = {
+        'hostname': socket.gethostname(),
+        'system': platform.platform(),
+        'release': platform.release(),
+        'version': platform.version()
+    }
     user_agent = request.headers.get('User-Agent')
-    return '<h1>Hola {}!</h1><p>Brougth to you from your browser {}</p>'.format(name, user_agent)
+    return render_template('index.html', user_agent=user_agent, name=name, host_info=host_info)
+
+@app.route('/cookie')
+def set_cookie():
+    response = make_response('<h1>This document carries a cookie!</h1>')
+    response.set_cookie('answer', '42')
+    return response
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hola, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
